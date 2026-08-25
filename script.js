18072975144055aac9abfdc800ed0a821328d048
alejandraperalta13525-bit <alejandraperalta13525@gmail.com>
2026-08-25 07:18:41 -0500

    Fix button onclick for Pomodoro functionality

--- script.js
+++ script.js
@@ -1041,7 +1041,7 @@ else {
 </p>
 
 ${tecnica.nombre === "Pomodoro" ? `
-    <button onclick="iniciarPomodoro()" style="
+    <button onclick="alert('Pomodoro funciona')" style="
         margin-top:15px;
         padding:10px 18px;
         border:none;
